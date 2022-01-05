4a68539f893816327e3a3ab7e7b912ddb422bc8e
Jaap Wiering <github.com@fanagalo.nl>
2022-01-05 16:40:16 +0100

    changed Google font call

--- header.php
+++ header.php
@@ -14,7 +14,10 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
-	<link href="https://fonts.googleapis.com/css?family=Roboto:400,400i,700,700i" rel="stylesheet">
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
+
 	<?php wp_head(); ?>
 </head>
 
